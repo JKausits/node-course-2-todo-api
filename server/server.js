bf763dbca5d39fd9f600e3d0e96f5f79b317f582
Joe Kausits <JoeKausits@gmail.com>
2017-08-06 22:18:00 -0400

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
+
 
 // Local Modules
 var {mongoose} = require('./db/mongoose');
@@ -85,6 +87,37 @@ app.delete('/todos/:id', (req, res)=>{
     res.status(400);
   });
 });
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    res.status(404).send(`${id} is not a valid Todo ID`);
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+      body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    // Check to see if a Todo was found and removed
+    if(!todo){
+      res.status(404).send(`No user could be found with the ID: ${id}`);
+    }
+
+    res.send({todo});
+  }).catch((err)=>{
+    res.send(400);
+  }
+  )
+
+});
+
 
 app.get('/', (req, res)=>{
   res.send('Express Server');
